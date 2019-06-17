1d3bb74dcfce9732348e9b0f74e6e9be59640db6
Hasanli Orkhan <46919226+OrkhanHI@users.noreply.github.com>
2019-06-16 20:40:54 -0400

    Add files via upload

---
+++ _utils.py
@@ -0,0 +1,24 @@
+import torch
+import os 
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append((int(correct_k),correct_k.mul_(1.0/batch_size)))
+        return res
+
+def find_classes(dir):
+    classes = os.listdir(dir)
+    classes.sort()
+    class_to_idx = {classes[i]: i for i in range(len(classes))}
+    return classes, class_to_idx
